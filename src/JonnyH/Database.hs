c8bba3553b328c28936f2e0d1f16a0c71f778f11
Ludvig Sundström <lud.sund@gmail.com>
2018-02-18 18:09:36 +0100

    Add function to filter games depending on initial movesequence

--- src/JonnyH/Database.hs
+++ src/JonnyH/Database.hs
@@ -1,8 +1,10 @@
 
 module JonnyH.Database where
 
+import           Data.List                  (foldr1)
+import           Data.Set                   hiding (filter, map)
 import           Database.PostgreSQL.Simple
-import           Protolude
+import           Protolude                  hiding (toList)
 
 import           PGNParser.Data.Metadata
 import           PGNParser.Data.MoveText
@@ -55,9 +57,47 @@ insertGame conn (Game meta moves) = do
     _ <- insertMoves conn i moves
     return ()
 
-rundb :: IO ()
-rundb = do
+populate :: IO ()
+populate = do
     conn <- makeConnection
     Right gs <- parseFile "resources/pgn/QGDOrthoMain.pgn"
     mapM_ (insertGame conn) gs
     return ()
+
+
+queryMove :: Connection -> Query -> MoveText -> IO [Only Int]
+queryMove conn q (Move n w b) = query conn q (n, w, b)
+queryMove _ _ _ = return []
+
+gamesWithMove :: Connection -> MoveText -> IO (Set Int)
+gamesWithMove conn move = do
+    let q = "select gameid from move where movenumber=? and white=? and black=?"
+    xs <- queryMove conn q move
+    return $ fromList $ map fromOnly xs
+
+
+gamesWithMoveSequence :: Connection -> [MoveText] -> IO (Set Int)
+gamesWithMoveSequence conn moves = do
+    games <- mapM (gamesWithMove conn) moves
+    return $ foldr1 intersection games
+
+dummy = [ Move 1 "d4" "d5"
+        , Move 2 "c4" "e6"
+        , Move 3 "Nc3" "Nf6"
+        , Move 4 "Bg5" "Be7"
+        , Move 5 "e3" "h6"
+        , Move 6 "Bh4" "O-O"
+        , Move 7 "Rc1" "b6"
+        ]
+
+-- assuming white to play
+respond moves color n = do
+    conn <- makeConnection
+    g <- gamesWithMoveSequence conn dummy
+    let q = "select white,count(white) \
+            \from move \
+            \where gameid in ? and movenumber=? group by white"
+        params = (In (toList g), n)
+    res <- query conn q params :: IO [(Text, Int)]
+    print res
+
