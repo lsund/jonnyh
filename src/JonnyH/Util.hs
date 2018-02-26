4c1f40bd74974ae4cc2fb9a0f7410c735e03c40b
Ludvig Sundström <lud.sund@gmail.com>
2018-02-26 09:07:40 +0100

    Add accumJust for folding over lists of maybe

--- src/JonnyH/Util.hs
+++ src/JonnyH/Util.hs
@@ -6,3 +6,7 @@ import           Protolude
 
 firstJust :: [Maybe a] -> Maybe a
 firstJust xs = fromMaybe Nothing (find isJust xs)
+
+accumJust :: Maybe a -> [a] -> [a]
+accumJust (Just x) acc = x : acc
+accumJust Nothing acc = acc
